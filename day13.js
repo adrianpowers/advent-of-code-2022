d14377ac470869ea830745c186b5cb577fa41685
Adrian Powers <adrianpowers530@gmail.com>
2022-12-13 02:40:25 -0500

    Day 13, works on sample input, not puzzle input

--- day13.js
+++ day13.js
@@ -2,43 +2,66 @@ let fs = require("fs");
 
 function distressed(text){
   const lines = text.split("\n").map((str) => str.trim()).map(line => eval(line));
-  console.log(lines)
   let isCorrectOrder = {};
   
   for(let p = 0; p < lines.length; p+=3){
     let left = lines[p];
     let right = lines[p+1];
 
+    // console.log(`PAIR ${p / 3 + 1}: ${left} , ${right}`)
     
-    // // base case - if all the items are integers, checking through them until we find out 
-    for(let i = 0; i < left.length; i++){
-      if(Number.isInteger(left[i]) && !Number.isInteger(right[i])){
-        left[i] = [left[i]];
-      } else if(!Number.isInteger(left[i]) && Number.isInteger(right[i])){
-        right[i] = [right[i]];
-      }
+    if(left.length === 0){
+      isCorrectOrder[(p / 3) + 1] = true;
+    }
+    
+    if(right.length === 0){
+      isCorrectOrder[(p / 3) + 1] = false;
+    }
 
-      console.log(left[i], right[i])
-      
-      // integer checking
-      if(left[i] > right[i]){
-        isCorrectOrder[(p / 3) + 1] = false;
-      } else if(left[i] < right[i]) {
-        isCorrectOrder[(p / 3) + 1] = true;
-      }
-
-      // checking lengths
-      if(left.length > right.length){
-        isCorrectOrder[(p / 3) + 1] = false;  
-      } else if(left.length < right.length){
-        isCorrectOrder[(p / 3) + 1] = true;
+    if(right.length > 0 && left.length > 0){
+      // // base case - if all the items are integers, checking through them until we find out 
+      for(let i = 0; i < left.length; i++){
+  
+        if(Number.isInteger(left[i]) && !Number.isInteger(right[i])){
+          left[i] = [left[i]];
+        } else if(!Number.isInteger(left[i]) && Number.isInteger(right[i])){
+          right[i] = [right[i]];
+        }
+        
+        // integer checking
+        if(left[i] > right[i]){
+          isCorrectOrder[(p / 3) + 1] = false;
+          break;
+        } else if(left[i] < right[i]) {
+          isCorrectOrder[(p / 3) + 1] = true;
+          break;
+        }
+  
+        // checking lengths
+        if(left.length > right.length){
+          isCorrectOrder[(p / 3) + 1] = false;  
+          break;
+        } else if(left.length < right.length){
+          isCorrectOrder[(p / 3) + 1] = true;
+          break;
+        }
       }
     }
   }
 
+  let sum = 0;
 
-  console.log(isCorrectOrder)
+  let indices = Object.keys(isCorrectOrder);
+  indices.forEach(index => {
+    if(isCorrectOrder[index]){
+      sum += Number(index);
+    }
+  })
+
+  return sum;
   
 }
+
+// 5210 is too low
 
 console.log(distressed(fs.readFileSync("./day13input.txt", { encoding: "utf-8" })));
